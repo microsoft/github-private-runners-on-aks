91489a99e99f5711e3b23e8973b0b2de7618c6b5
arnaud-tincelin <25585350+arnaud-tincelin@users.noreply.github.com>
2022-01-26 16:22:05 +0100

    Add var to enable/disable AGIC + doc

--- cluster_deployment/variables.tf
+++ cluster_deployment/variables.tf
@@ -3,3 +3,9 @@ variable "location" {
   description = "A location for the resources."
   default     = "westeurope"
 }
+
+variable "enable_agic" {
+  type        = bool
+  description = "True to enable Application Gateway Ingress Controller; otherwise false."
+  default     = true
+}
